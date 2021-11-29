88a75db08977c25c17a4db7b904f4d7b1e08d290
Nikita <nikita_dubyk@mail.ru>
2021-11-29 18:07:33 +0300

    add css transition group

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,7 +1,7 @@
 import { useHttp } from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import {
     heroesFetching,
     heroesFetched,
@@ -39,22 +39,27 @@ const HeroesList = () => {
 
     const renderHeroesList = arr => {
         if (arr.length === 0) {
-            return <h5 className='text-center mt-5'>Героев пока нет</h5>;
+            return (
+                <CSSTransition classNames='hero' timeout={0}>
+                    <h5 className='text-center mt-5'>Героев пока нет</h5>
+                </CSSTransition>
+            );
         }
 
         return arr.map(({ id, ...props }) => {
             return (
-                <HeroesListItem
-                    key={id}
-                    {...props}
-                    onDeleteHero={() => onDeleteHero(id)}
-                />
+                <CSSTransition classNames='hero' key={id} timeout={500}>
+                    <HeroesListItem
+                        {...props}
+                        onDeleteHero={() => onDeleteHero(id)}
+                    />
+                </CSSTransition>
             );
         });
     };
 
     const elements = renderHeroesList(filteredHeroes);
-    return <ul>{elements}</ul>;
+    return <TransitionGroup component='ul'>{elements}</TransitionGroup>;
 };
 
 export default HeroesList;
